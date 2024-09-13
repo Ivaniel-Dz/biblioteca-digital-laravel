664fbfaa77e59556d3405c63e355589245b14259
Ivaniel-Dz <pcivaniel@gmail.com>
2024-09-12 20:10:12 -0500

    update about

--- resources/js/Pages/LandingPage/Service.vue
+++ resources/js/Pages/LandingPage/Service.vue
@@ -4,37 +4,37 @@
         <div class="container mx-auto">
             <h2 class="text-3xl font-bold text-center mb-8">Nuestros Servicios</h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+
                 <!-- Servicio 1 -->
-                <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden custom-shadow">
+                <div v-for="service in servicios" :key="service.service"
+                class="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden custom-shadow">
                     <div class="p-6">
-                        <h3 class="text-xl font-semibold mb-4">Consulta Bibliográfica</h3>
+                        <h3 class="text-xl font-semibold mb-4"> {{ service.title }} </h3>
                         <p class="leading-relaxed">
-                            Ofrecemos asistencia personalizada para encontrar libros y recursos que se ajusten a tus
-                            necesidades.
+                            {{ service.description }}
                         </p>
                     </div>
                 </div>
-                <!-- Servicio 2 -->
-                <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden custom-shadow">
-                    <div class="p-6">
-                        <h3 class="text-xl font-semibold mb-4">Club de Lectura</h3>
-                        <p class="leading-relaxed">
-                            Únete a nuestros clubes de lectura para discutir tus libros favoritos con otros entusiastas.
-                        </p>
-                    </div>
-                </div>
-                <!-- Servicio 3 -->
-                <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden custom-shadow">
-                    <div class="p-6">
-                        <h3 class="text-xl font-semibold mb-4">Eventos y Talleres</h3>
-                        <p class="leading-relaxed">
-                            Participa en nuestros eventos y talleres educativos para mejorar tus habilidades de lectura
-                            y escritura.
-                        </p>
-                    </div>
-                </div>
+
             </div>
         </div>
     </section>
 
 </template>
+
+<script setup>
+    const servicios = [
+        {
+            title: "Consulta Bibliográfica",
+            description: "Ofrecemos asistencia personalizada para encontrar libros y recursos que se ajusten a tus necesidades."
+        },
+                {
+            title: "Club de Lectura",
+            description: "Únete a nuestros clubes de lectura para discutir tus libros favoritos con otros entusiastas."
+        },
+                {
+            title: "Eventos y Talleres",
+            description: "Participa en nuestros eventos y talleres educativos para mejorar tus habilidades de lectura y escritura."
+        },
+    ];
+</script>
