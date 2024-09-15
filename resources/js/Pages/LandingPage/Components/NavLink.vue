f0fa90651e08f3808959596d47e226a967df9c36
Ivaniel-Dz <pcivaniel@gmail.com>
2024-09-15 04:48:19 -0500

    update menu link

--- resources/js/Pages/LandingPage/Components/NavLink.vue
+++ resources/js/Pages/LandingPage/Components/NavLink.vue
@@ -12,13 +12,13 @@ import SubLink from './SubLink.vue';
         <ul :class="{ 'absolute': menuOpen, 'hidden': !menuOpen, 'md:flex': true, 'flex-col': true, 'md:flex-row': true }"
             class="z-10 bg-color-main dark:bg-color-dark w-full left-0 py-4 pl-7 top-16 text-base font-medium md:flex md:w-auto md:items-center md:gap-6">
             <li class="my-6 md:my-0">
-                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="#home">
+                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="/">
                     Inicio
                 </a>
             </li>
 
             <li class="my-6 md:my-0">
-                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="#books">
+                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="/#books">
                     Libros
                 </a>
             </li class="my-6 md:my-0">
@@ -29,13 +29,13 @@ import SubLink from './SubLink.vue';
             </li>
 
             <li class="my-6 md:my-0">
-                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="#services">
+                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="/#services">
                     Servicios
                 </a>
             </li>
 
             <li class="my-6 md:my-0">
-                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="#about">
+                <a class="text-color-txt dark:text-color-txt-dark transition hover:text-color-txt/75" href="/#about">
                     Nosotros
                 </a>
             </li>
