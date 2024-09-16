7fbd2f41e840ac8b94e812bd8a0734ec63e5e184
Ivaniel-Dz <pcivaniel@gmail.com>
2024-09-16 17:22:15 -0500

    update CreatePrestamo

--- resources/js/Pages/Prestamos/Create.vue
+++ resources/js/Pages/Prestamos/Create.vue
@@ -10,29 +10,20 @@
     </div>
 
     <form @submit.prevent="submitForm" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
-      <div class="relative">
-        <input v-model="nombre" type="text" id="nombre"
-          class="w-full rounded-lg border-gray-200 p-4 pt-6 text-sm shadow-sm focus:border-blue-500 focus:ring-blue-500 peer"
-          placeholder=" " />
-        <label for="nombre"
-          class="absolute left-3 -top-2 text-sm text-gray-500 bg-white px-1 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:left-4 peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:left-3 peer-focus:text-blue-500">
-          Nombre
-        </label>
-      </div>
 
       <div class="relative">
-        <input v-model="book.title" type="text" id="libro"
-          class="w-full rounded-lg border-gray-200 p-4 pt-6 text-sm shadow-sm focus:border-blue-500 focus:ring-blue-500 peer"
-          placeholder=" " />
+        <h3 id="libro" class="w-full rounded-lg border-gray-200 p-4 pt-6 text-sm shadow-sm"> 
+          {{form.libro }} 
+        </h3>
+
         <label for="libro"
           class="absolute left-3 -top-2 text-sm text-gray-500 bg-white px-1 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:left-4 peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:left-3 peer-focus:text-blue-500">
-          Libro a pedir
+          Libro a pedir {{ form.id }}
         </label>
       </div>
 
-      <div class="flex gap-2">
         <div class="relative">
-          <input v-model="fecha" type="date" id="fecha"
+          <input v-model="form.fecha" type="date" id="fecha"
             class="w-full rounded-lg border-gray-200 p-4 pt-6 text-sm shadow-sm focus:border-blue-500 focus:ring-blue-500 peer"
             placeholder=" " />
           <label for="fecha"
@@ -40,17 +31,6 @@
             Fecha de Prestamo
           </label>
         </div>
-
-        <div class="relative">
-          <input v-model="dias" type="number" id="dias"
-            class="w-full rounded-lg border-gray-200 p-4 pt-6 text-sm shadow-sm focus:border-blue-500 focus:ring-blue-500 peer"
-            placeholder=" " />
-          <label for="dias"
-            class="absolute left-3 -top-2 text-sm text-gray-500 bg-white px-1 transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:left-4 peer-placeholder-shown:text-gray-400 peer-focus:-top-2 peer-focus:left-3 peer-focus:text-blue-500">
-            Cantidad de Días a pedir
-          </label>
-        </div>
-      </div>
 
       <div class="flex items-center justify-between">
         <button type="submit" class="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white">
@@ -62,14 +42,17 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { ref } from 'vue';
 
 const props = defineProps({
   book: Object
 });
 
-const nombre = '';
-const fecha = '';
-const dias = '';
+const form = ref({
+  id: props.book.key.replace('/works/', ''),
+  libro: props.book.title,
+  fecha: "",
+});
+
 
 </script>
